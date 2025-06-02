95b96428a9fef2890e1add6d13d05348fe06ff72
Joshua <jwhite83193@gmail.com>
2025-06-02 18:29:13 -0500

    Changes Made Awhile Back, Never Pushed to My Repo

--- RED-GREEN-REFACTOR.test.js
+++ RED-GREEN-REFACTOR.test.js
@@ -1,7 +1,13 @@
 const hello = require('./RED-GREEN-REFACTOR.js');
+let invalidNumberInput = "Numbers may not be included with this input";
 
 describe('hello', function() {
 
+    test('should return a message to the user stating that numbers may not be included.',
+        function () {
+            expect(hello(Number())).toBe('INVALID INPUT: Numbers may not be included.');
+        });
+    
     test('should return a custom message when a name is provided',
         function () {
             expect(hello('Joshua')).toBe('Hi, Joshua!');
@@ -9,7 +15,7 @@ describe('hello', function() {
 
     test('should return a custom message when a name is provided',
         function () {
-            expect(hello('Joshua')).toBe('Hi, Joshua!');
+            expect(hello()).toBe('Hi, dude!');
         });
 
 });
